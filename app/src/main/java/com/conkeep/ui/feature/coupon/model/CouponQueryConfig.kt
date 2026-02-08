caf2e96a10911af1bbe7ca6ae45b11730d14f3f8
medAndro <med4ndro@gmail.com>
2026-02-09 00:16:36 +0900

    feat: 쿠폰 목록 기본 상태를 '사용 가능'으로 변경

--- app/src/main/java/com/conkeep/ui/feature/coupon/model/CouponQueryConfig.kt
+++ app/src/main/java/com/conkeep/ui/feature/coupon/model/CouponQueryConfig.kt
@@ -2,6 +2,6 @@ package com.conkeep.ui.feature.coupon.model
 
 data class CouponQueryConfig(
     val query: String = "",
-    val filter: CouponFilterType = CouponFilterType.ALL,
+    val filter: CouponFilterType = CouponFilterType.AVAILABLE,
     val sort: CouponSortType = CouponSortType.EXPIRY,
 )
