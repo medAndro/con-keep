88babef72b894dafa6f0c201e49bea31204d0a3c
medAndro <med4ndro@gmail.com>
2026-02-20 09:25:54 +0900

    feat: 버전 정보 업데이트 0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.conkeep"
         minSdk = 28
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.0.4"
+        versionCode = 8
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
