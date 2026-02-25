dd89316349d4b246064edae496c3097217ba9907
medAndro <med4ndro@gmail.com>
2026-02-25 12:53:48 +0900

    build: 앱 버전 업데이트 0.0.9 → 0.0.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.conkeep"
         minSdk = 28
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.0.9"
+        versionCode = 12
+        versionName = "0.0.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
