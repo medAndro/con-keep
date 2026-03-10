f76eec3b45ea34d8d80ab1473df87417f3aae1d5
medAndro <med4ndro@gmail.com>
2026-03-10 15:07:20 +0900

    refactor: CouponTypeConverters 위치 이동 (CouponDTO.kt -> CouponDatabase.kt)

--- app/src/main/java/com/conkeep/data/local/CouponDatabase.kt
+++ app/src/main/java/com/conkeep/data/local/CouponDatabase.kt
@@ -2,10 +2,11 @@ package com.conkeep.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import com.conkeep.data.local.dao.CouponDao
 import com.conkeep.data.local.entity.CouponEntity
-import com.conkeep.data.remote.dto.CouponTypeConverters
+import com.conkeep.ui.feature.coupon.model.CouponSortType
 
 @TypeConverters(CouponTypeConverters::class)
 @Database(
@@ -15,4 +16,12 @@ import com.conkeep.data.remote.dto.CouponTypeConverters
 )
 abstract class CouponDatabase : RoomDatabase() {
     abstract fun couponDao(): CouponDao
+}
+
+class CouponTypeConverters {
+    @TypeConverter
+    fun fromSortType(type: CouponSortType): Int = type.sortType
+
+    @TypeConverter
+    fun toSortType(value: Int): CouponSortType = CouponSortType.entries.find { it.sortType == value } ?: CouponSortType.RECENT_ADD
 }
