a462f9240ffabcfc1bcadfa14e9a9075cb52fc23
medAndro <med4ndro@gmail.com>
2026-03-21 01:41:11 +0900

    refactor: 알림 설정 컴포넌트를 BottomSheet에서 Dialog 방식으로 변경

--- app/src/main/java/com/conkeep/ui/feature/setting/SettingScreen.kt
+++ app/src/main/java/com/conkeep/ui/feature/setting/SettingScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -258,10 +257,7 @@ fun SettingScreenContent(
     }
 
     showSettingBottomSheet.value?.let { initAlarmSetting: CouponAlarmSetting ->
-        AlarmSettingBottomSheet(
-            onDismiss = {
-                showSettingBottomSheet.value = null
-            },
+        AlarmSettingDialog(
             onConfirm = { selectedAlarm ->
                 addCouponAlarmSetting(selectedAlarm)
                 showSettingBottomSheet.value = null
@@ -270,7 +266,6 @@ fun SettingScreenContent(
             onCancel = {
                 showSettingBottomSheet.value = null
             },
-            sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
         )
     }
 
