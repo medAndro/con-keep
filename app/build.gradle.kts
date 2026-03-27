714beea2186fe9acdac08233090ceefaf5696635
medAndro <med4ndro@gmail.com>
2026-03-27 23:36:48 +0900

    chore: 앱 버전 업데이트 (versionCode 19, versionName 0.9.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.conkeep"
         minSdk = 28
         targetSdk = 36
-        versionCode = 18
-        versionName = "0.9.0"
+        versionCode = 19
+        versionName = "0.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
